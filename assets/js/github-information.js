6a8e1f968c1f3939e9e1ec8e39834ca82be9d800
Alex <nypels.alexandre@outlook.com>
2021-12-03 00:17:08 +0000

    Adding GitHub API

---
+++ assets/js/github-information.js
@@ -0,0 +1,76 @@
+let userInformationHTML = (user) => {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
+            </span>
+        </h2>
+        <div class="gh-content">
+            <div class="gh-avatar">
+                <a href="${user.html_url}" target="_blank">
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                </a>
+            </div>
+            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+        </div>
+    `
+}
+
+let repoInformationHTML = (repos) => {
+    if(repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`
+    }
+    let listItemsHTML = repos.map((repo) => {
+        return `
+            <li>
+                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </li>
+        `;
+    })
+    return `
+        <div class="clearfix repo-list">
+            <p><strong>Repo List:</strong></p>
+            <ul>${listItemsHTML.join("\n")}</ul>
+        </div>
+    `;
+}
+
+let fetchGitHubInformation = (e) => {
+    $("#gh-user-data").html("")
+    $("#gh-repo-data").html("")
+    let username = $("#gh-username").val()
+    if(!username){
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`)
+        return
+    }
+    $("#gh-user-data").html(`
+        <div id="loader">
+            <img src="assets/css/loader.gif" alt="loading...">
+        </div>
+    `)
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        (firstResponse, secondResponse) => {
+            let userData = firstResponse[0]
+            let repoData = secondResponse[0]
+            $("#gh-user-data").html(userInformationHTML(userData))
+            $("#gh-repo-data").html(repoInformationHTML(repoData))
+        }, (errorResponse) => {
+            if(errorResponse.status === 404){
+                $("#gh-user-data").html(`<h2>User Not found</h2>`)
+            }else if(errorResponse.status === 403){
+                var resetTime = new Date(errorResponse.getResponseHeader("X-RateLimit-Reset") * 1000)
+                $("#gh-user-data").html(`<h4>Too many request, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
+            }
+            else{
+                console.log(errorResponse);
+                $("#gh-user-data").html(`<h2>${errorResponse.responseJSON.message}</h2>`)
+            }
+        }
+    )
+}
+
+$(document).ready(fetchGitHubInformation)
